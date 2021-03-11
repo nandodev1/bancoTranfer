88661b4fad675c148c05064b7feb774db18e8dcf
Fernando S. Gomes <nandobink6@gmail.com>
2021-03-10 21:12:51 -0300

    Criação dos arquivvos bae

---
+++ Conta/Conta.cs
@@ -0,0 +1,17 @@
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+        private TipoConta TipoConta {get; set;}
+        private double Saldo { get; set; }
+        private double Credito {get; set;}
+        public string Nome { get ; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c1 = new Conta();
+            c1.Nome = "João ozão";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
