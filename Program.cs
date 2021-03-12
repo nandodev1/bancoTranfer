be3137d26b3677a6548b4eafbe55f35654f54294
Fernando Gomes <nandobink6@gmail.com>
2021-03-12 01:39:13 +0000

    Terminado parte dos swits e menu principal

--- Program.cs
+++ Program.cs
@@ -12,41 +12,48 @@ namespace DIO.Bank
                     switch(opcaoUsuario)
                     {
                         case "1":
+                          // ListarConta();
                         break;
                         case "2":
+                          // InserirConta();
                         break;
                         case "3":
+                          // Transferir();
                         break;
                         case "4":
+                          // Sacar(); 
                         break;
                         case "5":
+                          // Depositar(); 
                         break;
                         case "C":
-                        break;
-                        case "X":
+                          // Console.clear();
                         break;
 
                         default:
-                            Console.WriteLine("Valor digitado invalido");
+                            throw new ArgumentOutOfRangeException();
                     }
+                    opcaoUsuario = ObterOpcaoUsuario();
                 }
+                Console.WriteLine("Obrigado por ultilizar nossos serviços.");
+                Console.ReadLine();
             
         }
         private static string ObterOpcaoUsuario()
             {
                 Console.WriteLine();
-                Console.WriteLine("DIO Bank a eu dispor!!!");
+                Console.WriteLine("DIO Bank a eu dispor!!!\n");
                 Console.WriteLine("Informe a opção desejada");
                 
                 Console.WriteLine("1- Listar conta");
                 Console.WriteLine("2- Inerir nova conta");
-                Console.WriteLine("3- Transsferir");
+                Console.WriteLine("3- Transferir");
                 Console.WriteLine("4- sacar");
                 Console.WriteLine("5- Depositar");
                 Console.WriteLine("c- Linpar tela");
                 Console.WriteLine("x- Sair");
 
-                string opcaoUsuario = Console.ReadLine().ToUpper;
+                string opcaoUsuario = Console.ReadLine().ToUpper();
                 return opcaoUsuario;
             }
     }
