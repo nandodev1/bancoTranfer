c0adc33a93f167630da363eb750b1073a35ecbdc
nandodev1 <nandobink6@gmail.com>
2021-03-11 21:24:46 -0300

    Preparando arquivos base

--- Conta/Conta.cs
+++ Conta/Conta.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.Bank
 {
     public class Conta
@@ -13,5 +15,40 @@ namespace DIO.Bank
         private double Saldo { get; set; }
         private double Credito {get; set;}
         public string Nome { get ; set;}
+        public bool Sacar(double valorSaque)
+        
+        {
+            if(this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                this.Saldo -= valorSaque;
+                Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);   
+            }
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDetino)
+        {
+            if(this.Sacar(valorTransferencia))
+            {
+                contaDetino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "|";
+            retorno += " TipoConta " + this.TipoConta;
+            retorno += " | Saldo " + this.Saldo;
+            retorno += " | Credito " + this.Credito;
+            retorno += " | Nome " + this.Nome;            
+            return retorno;
+        }
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,46 @@ namespace DIO.Bank
     {
         static void Main(string[] args)
         {
-            Conta c1 = new Conta();
-            c1.Nome = "João ozão";
-            Console.WriteLine("Hello World!");
+                string opcaoUsuario = ObterOpcaoUsuario();
+                while(opcaoUsuario.ToUpper() != "X")
+                {
+                    switch(opcaoUsuario)
+                    {
+                        case "1":
+                        break;
+                        case "2":
+                        break;
+                        case "3":
+                        break;
+                        case "4":
+                        break;
+                        case "5":
+                        break;
+                        case "C":
+                        break;
+                        case "":
+                        break;
+
+                        default:
+                            Console.WriteLine("Valor digitado invalido");
+                    }
+                }
+            
         }
+        private static string ObterOpcaoUsuario()
+            {
+                Console.WriteLine();
+                Console.WriteLine("DIO Bank a eu dispor!!!");
+                Console.WriteLine("Informe a opção desejada");
+                
+                Console.WriteLine("1- Listar conta");
+                Console.WriteLine("2- Inerir nova conta");
+                Console.WriteLine("3- Transsferir");
+                Console.WriteLine("4- sacar");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                return "x";
+            }
     }
 }
